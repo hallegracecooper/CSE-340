da50e2cd62bfdbf26999f9df090d2bf13d94349d
hallegracecooper <dri19003@byui.edu>
2025-03-31 18:36:08 -0600

    W4.2

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,13 +4,20 @@ const express = require("express");
 const router = express.Router();
 const utilities = require("../utilities");
 const accountController = require("../controllers/accountController"); // build this later
+const regValidate = require('../utilities/account-validation');
+
 
 // GET route for the login view
 // This route responds to "/account/login" since "account" is prefixed in server.js
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
-// POST route for handling account registration
-router.post('/register', utilities.handleErrors(accountController.registerAccount));
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  );  
 
 module.exports = router;
